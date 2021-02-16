68d8b9ab5e4126d41ed3fc0d4781ffdb8f404d9a
ldelima <delimce@gmail.com>
2021-02-16 23:39:56 +0100

    Rekorbit dashboard endpoint with svg files

--- src/modules/app/utils.js
+++ src/modules/app/utils.js
@@ -1,1 +1,33 @@
 'use strict';
+const svg64 = require('svg64');
+const fs = require("fs");
+
+module.exports =
+{
+    findCoins(cmcCoins, coins) {
+        return cmcCoins.filter((el) => {
+            return coins.includes(el.symbol);
+        })
+    },
+    goldPriceGram(oz_price) {
+        const gramxOz = 0.035274;
+        return gramxOz * oz_price
+    },
+    getQuantityRelBTC(btc, altcoin) {
+        let finalPrice =
+            altcoin.symbol === "BTC"
+                ? 1
+                : Number(altcoin.price_usd / btc.price_usd).toFixed(8);
+        return Number(finalPrice);
+    },
+    getSVGimage(image) {
+        let path = "./public/svg/" + image + ".svg";
+        let svg = false;
+        if (fs.existsSync(path)) {
+            svg = fs.readFileSync(path, 'utf-8');
+        } else {
+            svg = fs.readFileSync("./public/svg/auto.svg", 'utf-8');
+        }
+        return svg64(svg);
+    }
+}
--- src/routes/app.js
+++ src/routes/app.js
@@ -9,15 +9,76 @@ const utils = require("../modules/app/utils");
 
 router.post('/dashboard', async function (req, res) {
 
-    let coins = await req.body;
-     //multiple call
-     const [coinMarketCap, dolartoday, floatrates] = await Promise.all([
-      self.cmcAll(),
-      self.dtodayInfo(),
-      self.floatrates()
-    ]);
+  let coins = await req.body;
+  //multiple call
+  const [coinMarketCap, dolartoday, floatrates] = await Promise.all([
+    self.cmcAll(),
+    self.dtodayInfo(),
+    self.floatrates()
+  ]);
 
-    res.send(coinMarketCap);
-  });
+  let selectedCoins = await utils.findCoins(coinMarketCap, coins)
+  let price_gold_gram = await utils.goldPriceGram(dolartoday.GOLD.rate);
+  let btcCoin = await selectedCoins.find(el => { return el.symbol === "BTC" });
+  let floa_euro = await floatrates.find(el => { return el.code === "EUR" });
+  let floa_ars = await floatrates.find(el => { return el.code === "ARS" });
+  let currency = selectedCoins.map(el => {
+    return {
+      id: el.id,
+      cmc_id: el.cmc_id,
+      symbol: el.symbol,
+      type: "crypto",
+      typrice_btc: "crypto",
+      price_btc: utils.getQuantityRelBTC(btcCoin, el),
+      price_usd: el.price_usd,
+      percent4rent: el.percent4rent,
+      profit: el.profit,
+      image: utils.getSVGimage(el.symbol.toLowerCase())
+    };
+  })
+
+  //dolartoday data
+  let dollar = {
+    id: "dollar",
+    symbol: "USD",
+    type: "fiat",
+    price_bs: Number(dolartoday.USD.dolartoday),
+    price_usd: 1
+  };
+
+  currency.push(dollar);
+
+  let euro = {
+    id: "euro",
+    symbol: "EUR",
+    type: "fiat",
+    price_bs: Number(dolartoday.EUR.dolartoday),
+    price_usd: floa_euro.inverseRate
+  };
+
+  currency.push(euro);
+
+  let arg = {
+    id: "arg",
+    symbol: "ARS",
+    type: "fiat",
+    price_bs: Number(floa_ars.inverseRate * dolartoday.USD.dolartoday),
+    price_usd: floa_ars.inverseRate
+  };
+
+  currency.push(arg);
+
+  let gold = {
+    id: "gold",
+    symbol: "GOLD",
+    type: "commodity",
+    price_bs: Number(dolartoday.USD.dolartoday * price_gold_gram),
+    price_usd: Number(price_gold_gram)
+  };
+
+  currency.push(gold);
+
+  res.json(currency);
+});
 
 module.exports = router;
