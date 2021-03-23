00321610a105167f92dc9f40d92b93fe36d15f3b
ldelima <delimce@gmail.com>
2021-03-23 19:01:40 +0100

    reformat arg bluelytics endpoint response

--- src/modules/fiat/bluelytics.js
+++ src/modules/fiat/bluelytics.js
@@ -12,19 +12,32 @@ module.exports =
                 timeout: 40000,
                 url: 'https://api.bluelytics.com.ar/v2/latest'
             })
-            return this.getArsCurrencies(result.data);
+            let results = await this.getArsCurrencies(result.data);
+            return this.formatResults(results);
         } catch (error) {
             console.log(error)
             return result
         }
     },
-    getArsCurrencies: function (json) {
+    async getArsCurrencies(json) {
         let result = [];
         json.oficial.name = "official";
         json.blue.name = "blue";
         result.push(json.oficial);
         result.push(json.blue);
         return result;
+    },
+    formatResults(results) {
+        return results.map(el => {
+            return {
+                id: "ARG_" + String(el.name),
+                name: el.name,
+                symbol: 'ARG',
+                precent: 0.0,
+                price_sell: el.value_sell,
+                price_buy: el.value_buy
+            };
+        })
     }
 
 }
