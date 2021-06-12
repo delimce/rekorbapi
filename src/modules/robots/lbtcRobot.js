c98b51a56614489e2180e79d8b1d664637c1f268
ldelima <delimce@gmail.com>
2021-06-12 23:10:00 +0200

    Robot lbtc post send email

--- src/modules/robots/lbtcRobot.js
+++ src/modules/robots/lbtcRobot.js
@@ -3,6 +3,7 @@ const localbtcModule = require('../crypto/localbtc')
 const localBtcModel = require('../../models/localBtc')
 const utils = require('../app/utils');
 const geckoModule = require('../crypto/coingecko');
+const dtoday = require('../fiat/dolarToday');
 const currencies = require('../../../public/enums/currencies.json');
 const email = require('../app/email');
 const jsrender = require('jsrender');
@@ -10,26 +11,34 @@ const jsrender = require('jsrender');
 module.exports = {
     findPosts: async function () {
         const posts = await localBtcModel.find({ 'notified': false });
-        let results = {
-            posts: posts.length,
+        let state = {
+            posts: 0,
             notified: 0,
-            time: 0.0
-        };
+            time: 0.0,
+        }
+        state.posts = posts.length;
+        const startTime = Date.now()
         if (posts.length > 0) {
-            posts.forEach(async el => {
+            let i = 0;
+            while (i < posts.length) {
+                let el = posts[i];
                 let postData = await this.findFirstResultPost(el);
-                let updateData = { attempt: el.attempt + 1, }
+                let updateData = { attempt: el.attempt + 1 }
                 if (postData) {
                     //notify user
-                    let mail = el.user.get('email')
+                    let userEmail = el.user.get('email')
                     updateData.notified = true;
-                    results.notified += 1;
+                    state.notified += 1;
+                    this.notifyWithEmail(postData, userEmail);
                 }
                 //saving attempt & status
-                await localBtcModel.findByIdAndUpdate({ _id: el._id }, updateData)
-            });
+                await localBtcModel.findByIdAndUpdate({ _id: el._id }, updateData);
+                i++;
+            }
         }
-        return results;
+        const stopTime = Date.now()
+        state.time = (stopTime - startTime) / 1000;
+        return state;
     },
     findFirstResultPost: async function (search) {
         const country = utils.getCountryByCode(search.location).name;
@@ -41,6 +50,7 @@ module.exports = {
                 return ((post.bank && utils.anyElementsInText(post.bank.toLowerCase(), search.bank.toLowerCase()))
                     || (post.msg && utils.anyElementsInText(post.msg.toLowerCase(), search.bank.toLowerCase())))
                     && (search.amount >= post.min && search.amount <= post.max)
+                    && this.profitPrice(search, post, btcPrice) && post.profile.last_online === "ONLINE"
             });
         }
         return result;
@@ -49,22 +59,42 @@ module.exports = {
         let price = 0;
         const btcPriceData = await geckoModule.getPricesByIds(['bitcoin']);
         switch (currency) {
-            case currencies.usd:
-            case currencies.eur:
-                price = Number(btcPriceData[currency]);
+            case currencies[0].id: //usd
+            case currencies[1].id: //eur
+                price = btcPriceData.bitcoin[currency];
                 break;
-            case currencies.ves:
-                let vesFactor = 1000;
-                price = Number(btcPriceData[currency]) * vesFactor;
+            case currencies[2].id://ves
+                let vesFactor = await dtoday.getUsdPrice();
+                price = Number(btcPriceData.bitcoin[currencies[0].id]) * vesFactor;
                 break;
             default:
-                price = Number(btcPriceData[currencies.usd]);
+                price = Number(btcPriceData.bitcoin[currencies[0].id]); //usd
                 break;
         }
-        return price;
+        return Math.trunc(price);
+    }, getPercentageFeeBtc(postPrice, btcPrice) {
+        let price = Number(postPrice) / Number(btcPrice)
+        let x1 = Number(price.toFixed(2));
+        let percent = 0;
+        if (x1 >= 1) {
+            let int_part = Math.trunc(x1);
+            percent = x1 - int_part; //UP
+        } else {
+            percent = (1 - x1) * -1 //DOWN; 
+        }
+        return Number((percent).toFixed(2) * 100);
+    },
+    profitPrice(search, post, btcPrice) {
+        return (search.type === 'BUY') ?
+            (this.getPercentageFeeBtc(post.price, btcPrice) <= search.fee)
+            : (this.getPercentageFeeBtc(post.price, btcPrice) >= (search.fee * -1));
     },
     notifyWithEmail: async function (post, userEmail) {
-        email.setSubject("Localbitcoins search of posts completed");
+        email.setSubject("Your Localbitcoins post has been Found");
         email.setTo(userEmail);
+        let template = jsrender.templates('./src/templates/emails/lbtcposts.html');
+        let html = template.render(post);
+        email.setHtml(html);
+        await email.send();
     }
 };
--- src/routes/robots.js
+++ src/routes/robots.js
@@ -4,7 +4,7 @@ const lbtcRobot = require('../modules/robots/lbtcRobot')
 let router = express.Router();
 
 router.get('/localbtc/posts', async function (req, res) {
-        try {
+        try {  
                 let result = await lbtcRobot.findPosts();
                 res.json(result);
         } catch (err) {
